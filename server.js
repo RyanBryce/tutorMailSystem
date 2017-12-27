817a3d397645737c8d5ec0ed4ca29eacf1e847ed
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-12-27 15:34:41 -0700

    added callback that will run after userid is established. working on creating meeting

--- server.js
+++ server.js
@@ -8,30 +8,58 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+//express middlewear
 app.use(express.static('public'))
 app.use(cors())
 app.use(bodyParser.json());
 
-// console.log(process.env)
+//This automatically creates header, and returns JWT
 var payload = {
   iss: process.env.ZOOMAPIKEY,
-  exp: ((new Date()).getTime() + 5000)
+  exp: ((new Date()).getTime() + 300000)
 };
 
-//Automatically creates header, and returns JWT
-var userId = 722429;
 var token = jwt.sign(payload, process.env.ZOOMSECRET);
 console.log(token)
 
+//this is used for creating an auth preflight header to grab your account info from Zoom.us.
 var options = {
   url: `https://api.zoom.us/v2/users/`,
   headers: {
     'Authorization': `Bearer${token}`
   }
 };
-request(options, function (err, response, body) {
-  console.log(body)
 
+var userId;
+function getUserId(cb) {
+  request(options, function (err, response, body) {
+    var info = JSON.parse(body);
+    console.log(info)
+    userId = info.users[0].id
+    cb()
+  })
+
+}
+
+//url for creating a meeting
+//POST https://api.zoom.us/v2/users/{userId}/meetings
+getUserId(function(){
+  console.log(userId)
+
+  var options = {
+    url: `https://api.zoom.us/v2/users/${userId}/meetings`,
+    headers: {
+      'Authorization': `Bearer${token}`
+    },
+    method: "POST",
+    body: JSON.stringify({
+      topic: "ryans node hit"
+    })
+  };
+  request(options, function (err, response, body) {
+    var info = JSON.parse(body);
+    console.log(info)
+  })
 })
 
 
