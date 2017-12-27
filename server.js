33842504c15bcade1c39fd640b3ffb1025fc47b3
Ryan Bryce <ryan.bryce1992@gmail.com>
2017-12-27 14:09:44 -0700

    hittimg zoom api with preflight auth header. able to query data for my account now

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
+var request = require('request');
 
 var app = express();
 
@@ -11,15 +12,28 @@ app.use(express.static('public'))
 app.use(cors())
 app.use(bodyParser.json());
 
-console.log(process.env)
+// console.log(process.env)
 var payload = {
   iss: process.env.ZOOMAPIKEY,
-  exp: ((new Date()).getTime() + 50000)
+  exp: ((new Date()).getTime() + 5000)
 };
 
 //Automatically creates header, and returns JWT
+var userId = 722429;
 var token = jwt.sign(payload, process.env.ZOOMSECRET);
 console.log(token)
+
+var options = {
+  url: `https://api.zoom.us/v2/users/`,
+  headers: {
+    'Authorization': `Bearer${token}`
+  }
+};
+request(options, function (err, response, body) {
+  console.log(body)
+
+})
+
 
 require('./routes/routes.js')(app)
 
